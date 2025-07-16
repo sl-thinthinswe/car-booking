a98fd769ee1915e4d0d2b5e0e94db71f98fb3988
maymadisoe <maymadisoe66@gmail.com>
2025-07-16 16:54:41 +0630

    update customer route

--- routes/customer/web.php
+++ routes/customer/web.php
@@ -3,11 +3,6 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-
-
-
-
-
 Route::get("home", function () {
     return view("pages.customer.home");
 })->name('home');;
