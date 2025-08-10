6b1ab0a02a08919205e9a24e320c0b75df6f7f65
maymadisoe <maymadisoe66@gmail.com>
2025-08-10 18:54:49 +0630

    resolve conflict

---
+++ database/migrations/2025_08_10_121425_create_vehicles_table.php
@@ -0,0 +1,36 @@
+
+<?php
+// In the created migration file
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->id();
+            $table->string('license_plate')->unique();
+            $table->enum('model', ['small_car', 'express']);
+            $table->integer('seat_count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
